f0403149fea913817d834e2bfea7e74fd950da16
bhavya <thebhavyaahuja@gmail.com>
2026-06-21 21:09:05 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        path=[]
+        ans=[]
+        cols=set()
+        dia1=set()
+        dia2=set()
+        def dfs(row):
+            if row==n:
+                board=[]
+                for c in path:
+                    s='.'*c + 'Q'+'.'*(n-c-1)
+                    board.append(s)
+                ans.append(board)
+            for col in range(n):
+                if col not in cols and row-col not in dia1 and row+col not in dia2:
+                    path.append(col)
+                    cols.add(col)
+                    dia1.add(row-col)
+                    dia2.add(row+col)
+                    dfs(row+1)
+                    path.pop()
+                    cols.remove(col)
+                    dia1.remove(row-col)
+                    dia2.remove(row+col)
+        dfs(0)
+        return ans
+            
